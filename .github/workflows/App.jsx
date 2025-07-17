d0f317bfe605d6a2ea290e0c1aec7846858b93a6
santoansh <asa.santo@gmail.com>
2025-07-17 10:16:34 +0530

    Add files via upload

---
+++ .github/workflows/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>🚀 नमस्ते BRO! सपना अब अधूरा नहीं रहा</h1>
+      <p>React + Vite प्रोजेक्ट Live है 💻</p>
+    </div>
+  )
+}
+
+export default App
